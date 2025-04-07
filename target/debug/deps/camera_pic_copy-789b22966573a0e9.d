63543110a17c23b64875659eba9826a6514c7796
nino <1005963022@qq.com>
2025-04-07 13:59:52 +0800

    添加gitignore

--- target/debug/deps/camera_pic_copy-789b22966573a0e9.d
+++
@@ -1,5 +0,0 @@
-/Users/nino/CLionProjects/Rust_System/camera_pic_copy/target/debug/deps/camera_pic_copy-789b22966573a0e9: src/main.rs
-
-/Users/nino/CLionProjects/Rust_System/camera_pic_copy/target/debug/deps/camera_pic_copy-789b22966573a0e9.d: src/main.rs
-
-src/main.rs:
