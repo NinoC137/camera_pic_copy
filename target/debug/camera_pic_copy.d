d853dcf8c8e75459ad2645febfbf2a005f847c1b
nino <1005963022@qq.com>
2025-04-07 14:48:50 +0800

    rm files

--- target/debug/camera_pic_copy.d
+++
@@ -1,1 +0,0 @@
-/Users/nino/CLionProjects/Rust_System/camera_pic_copy/target/debug/camera_pic_copy: /Users/nino/CLionProjects/Rust_System/camera_pic_copy/src/main.rs
--- target/debug/deps/camera_pic_copy-1a3b67d71cfc2ab9.d
+++
@@ -1,5 +0,0 @@
-/Users/nino/CLionProjects/Rust_System/camera_pic_copy/target/debug/deps/camera_pic_copy-1a3b67d71cfc2ab9: src/main.rs
-
-/Users/nino/CLionProjects/Rust_System/camera_pic_copy/target/debug/deps/camera_pic_copy-1a3b67d71cfc2ab9.d: src/main.rs
-
-src/main.rs:
